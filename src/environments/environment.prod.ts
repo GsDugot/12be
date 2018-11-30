221f50b6aa80be8ea94378cb0dd94f072ab6f577
AngelUrq <angel4441916@gmail.com>
2018-11-29 23:42:40 -0400

    Conexión de firebase en producción y configuración inicial para el hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyC0OWyJ90WnAjFJ79q5Y_68C_hUs-oUl4U",
+    authDomain: "be-12-be.firebaseapp.com",
+    databaseURL: "https://be-12-be.firebaseio.com",
+    projectId: "be-12-be",
+    storageBucket: "be-12-be.appspot.com",
+    messagingSenderId: "989633311562"
+  }
 };
